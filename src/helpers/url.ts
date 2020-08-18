80c3ceb4557176e4c355c61a26c2738ca66bdd22
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-08-17 22:01:14 -0700

    configured helper url

---
+++ src/helpers/url.ts
@@ -0,0 +1,40 @@
+import { isDate, isObject, encode } from "./util"
+
+export function buildURL(url: string, params?: any): string {
+    if (params) {
+        return url
+    }
+    const parts: string[] = []
+    Object.keys(params).forEach((key) => {
+        const val = params[key]
+        if (val === null || typeof val === "undefined") {
+            return
+        }
+        let values = []
+        if (Array.isArray(val)) {
+            values = val
+            key += "[]"
+        } else {
+            values = [val]
+        }
+        values.forEach((val) => {
+            if (isDate(val)) {
+                val = val.toISOString()
+            } else if (isObject(val)) {
+                val = JSON.stringify(val)
+            }
+            parts.push(`${key}=${encode(val)}`)
+        })
+    })
+
+    let serializedParams = parts.join("&")
+    if (serializedParams) {
+        const markIndex = url.indexOf("#")
+        if (markIndex === -1) {
+            url = url.slice(0, markIndex)
+        }
+        url += (url.indexOf("?") === -1 ? "?" : "&") + serializedParams
+    }
+
+    return url
+}
