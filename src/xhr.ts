cc94f3bfb6a9e5fa8010f3cedbe7febd554eff9c
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-08-19 05:41:00 -0700

    feat(example/base src/index src/xhr src/helpers/index): request response handling
    set AxiosResponse & AxiosPromise interface, return axios promised response, and set test for
    response

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,16 +1,53 @@
-import { AxiosRequestConfig } from "./types";
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "./types";
 
-export default function xhr(config: AxiosRequestConfig): void {
-	const { data = null, url, method = "GET", headers } = config;
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+	return new Promise((resolve, reject) => {
+		const { data = null, url, method = "GET", headers, responseType } = config;
 
-	const request = new XMLHttpRequest();
-	request.open(method.toUpperCase(), url, true);
-	Object.keys(headers).forEach((name) => {
-		if (data === null && name.toLowerCase() === "content-type") {
-			delete headers[name];
-		} else {
-			request.setRequestHeader(name, headers[name]);
+		const request = new XMLHttpRequest(); // initial a standard XMLHttpRequest
+
+		/*
+		 *  handle promised request response
+		 * */
+		if (responseType) {
+			// get responseType from config
+			request.responseType = responseType;
 		}
+
+		request.open(method.toUpperCase(), url, true); // set request method and config URL
+
+		request.onreadystatechange = function handleLoad() {
+			// Ajax request status
+			if (request.readyState !== 4) {
+				// if server has received
+				return null;
+			}
+
+			const responseHeaders = request.getAllResponseHeaders();
+			const responseData = responseType !== "text" ? request.response : request.responseText;
+			const response: AxiosResponse = {
+				// initial response object
+				data: responseData,
+				status: request.status,
+				statusText: request.statusText,
+				headers: responseHeaders,
+				config,
+				request,
+			};
+			resolve(response);
+		};
+
+		/*
+		 *  handle setting request header
+		 * */
+
+		Object.keys(headers).forEach((name) => {
+			if (data === null && name.toLowerCase() === "content-type") {
+				delete headers[name];
+			} else {
+				request.setRequestHeader(name, headers[name]);
+			}
+		});
+		request.send(data);
 	});
-	request.send(data);
 }
