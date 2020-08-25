e11a48ddc8056b38a016cded963409418e36ffcf
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-08-25 04:47:58 -0700

    test(examples/extend/app): tests for generic response data type
    tests for generic response data type

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,5 +1,9 @@
 import axios from "../../src/index";
 
+/***********************************************************************************************************************
+ * 													text for axios base interfaces      						       *
+ * *********************************************************************************************************************/
+
 axios({
 	url: "/extend/post",
 	method: "post",
@@ -30,6 +34,10 @@ axios.put("/extend/put", { msg: "put" });
 
 axios.patch("/extend/patch", { msg: "patch" });
 
+/***********************************************************************************************************************
+ * 													text for overload axios request      						       *
+ * *********************************************************************************************************************/
+
 axios({
 	url: "/extend/post",
 	method: "post",
@@ -44,3 +52,33 @@ axios("/extend/post", {
 		msg: "hello",
 	},
 });
+
+/***********************************************************************************************************************
+ * 													text for generic response data type 						       *
+ * *********************************************************************************************************************/
+
+interface ResponseData<T = any> {
+	code: number;
+	result: T;
+	message: string;
+}
+
+interface User {
+	name: string;
+	age: number;
+}
+
+function getUser<T>() {
+	return axios<ResponseData<T>>("/extend/user")
+		.then((res) => res.data)
+		.catch((err) => console.error(err));
+}
+
+async function test() {
+	const user = await getUser<User>();
+	if (user) {
+		console.log(user.result.name);
+	}
+}
+
+test();
