aba7c0478a9380e56894a23591878114dbb83f10
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-08-26 20:31:44 -0700

    feat(src/defaults | axios | core/Axios | types/index): added default configs and applied to axios
    added default configs and applied to axios

--- src/types/index.ts
+++ src/types/index.ts
@@ -44,6 +44,7 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+	defaults: AxiosRequestConfig;
 	interceptors: {
 		request: AxiosInterceptorManger<AxiosRequestConfig>;
 		response: AxiosInterceptorManger<AxiosResponse>;
